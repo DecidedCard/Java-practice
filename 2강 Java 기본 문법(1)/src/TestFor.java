3629224b57de7ca66238c5dafeba43f3dcbee663
heajun <heajun35@gmail.com>
2026-02-28 23:35:49 +0900

    Feat: 2강 Java 기본 문법 1

---
+++ 2강 Java 기본 문법(1)/src/TestFor.java
@@ -0,0 +1,10 @@
+public class TestFor {
+    public static void main(String[] args) {
+        int[] a = { 32, 87, 3, 589, 12 };
+
+        for (int i : a) {
+            System.out.println(i + " ");
+        }
+    }
+
+}
